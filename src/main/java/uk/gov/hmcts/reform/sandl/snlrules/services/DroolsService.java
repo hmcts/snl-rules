6f4b045cb4a04d8752bcf38c87074c8041ef72fc
Kamil Pakur <k.pakur@kainos.com>
2018-04-16 13:55:09 +0200

    SL-744 existing test fixed and uncommented creation of drools object

--- src/main/java/uk/gov/hmcts/reform/sandl/snlrules/services/DroolsService.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlrules/services/DroolsService.java
@@ -1,30 +1,35 @@
 package uk.gov.hmcts.reform.sandl.snlrules.services;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
+import uk.gov.hmcts.reform.sandl.snlrules.drools.TrackingFactsChangedEventListener;
+import uk.gov.hmcts.reform.sandl.snlrules.drools.TrackingProcessStatusEventListener;
+import uk.gov.hmcts.reform.sandl.snlrules.drools.TrackingRulesFiredEventListener;
 
 
 @Service
 @ApplicationScope
 public class DroolsService {
-    private KieSession rulesSession;
-//    private final TrackingRulesFiredEventListener trackingRulesFiredEventListener;
-//    private final TrackingFactsChangedEventListener trackingFactsChangedEventListener;
-//    private final TrackingProcessStatusEventListener trackingProcessStatusEventListener;
+    private final KieSession rulesSession;
+    private final TrackingRulesFiredEventListener trackingRulesFiredEventListener;
+    private final TrackingFactsChangedEventListener trackingFactsChangedEventListener;
+    private final TrackingProcessStatusEventListener trackingProcessStatusEventListener;
 
     public DroolsService() {
         // load up the knowledge base
-//        KieServices kieServices = KieServices.Factory.get();
-//        KieContainer kieContainer = kieServices.getKieClasspathContainer();
-//        rulesSession = kieContainer.newKieSession("ksession-rules");
-//
-//        trackingRulesFiredEventListener = new TrackingRulesFiredEventListener();
-//        trackingFactsChangedEventListener = new TrackingFactsChangedEventListener();
-//        trackingProcessStatusEventListener = new TrackingProcessStatusEventListener();
-//        rulesSession.addEventListener(trackingRulesFiredEventListener);
-//        rulesSession.addEventListener(trackingFactsChangedEventListener);
-//        rulesSession.addEventListener(trackingProcessStatusEventListener);
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        rulesSession = kieContainer.newKieSession("ksession-rules");
+
+        trackingRulesFiredEventListener = new TrackingRulesFiredEventListener();
+        trackingFactsChangedEventListener = new TrackingFactsChangedEventListener();
+        trackingProcessStatusEventListener = new TrackingProcessStatusEventListener();
+        rulesSession.addEventListener(trackingRulesFiredEventListener);
+        rulesSession.addEventListener(trackingFactsChangedEventListener);
+        rulesSession.addEventListener(trackingProcessStatusEventListener);
 
         /*
         PackageBuilder packageBuilder = new PackageBuilder();
@@ -44,15 +49,15 @@ public class DroolsService {
         return rulesSession;
     }
 
-//    public TrackingRulesFiredEventListener getTrackingRulesFiredEventListener() {
-//        return trackingRulesFiredEventListener;
-//    }
-//
-//    public TrackingFactsChangedEventListener getTrackingFactsChangedEventListener() {
-//        return trackingFactsChangedEventListener;
-//    }
-//
-//    public TrackingProcessStatusEventListener getTrackingProcessStatusEventListener() {
-//        return trackingProcessStatusEventListener;
-//    }
+    public TrackingRulesFiredEventListener getTrackingRulesFiredEventListener() {
+        return trackingRulesFiredEventListener;
+    }
+
+    public TrackingFactsChangedEventListener getTrackingFactsChangedEventListener() {
+        return trackingFactsChangedEventListener;
+    }
+
+    public TrackingProcessStatusEventListener getTrackingProcessStatusEventListener() {
+        return trackingProcessStatusEventListener;
+    }
 }
--- src/test/java/uk/gov/hmcts/reform/sandl/snlrules/controllers/GetWelcomeTest.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlrules/controllers/GetWelcomeTest.java
@@ -1,47 +1,23 @@
 package uk.gov.hmcts.reform.sandl.snlrules.controllers;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import uk.gov.hmcts.reform.sandl.snlrules.services.DroolsService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebMvcTest
-@ContextConfiguration
-@WebAppConfiguration
+@WebMvcTest(RootController.class)
 public class GetWelcomeTest {
 
     @Autowired
-    private WebApplicationContext context;
-
-    @Mock
-    private DroolsService droolService;
-
-    //@Autowired
     private MockMvc mockMvc;
-
-    @Before
-    public void setup() {
-        mockMvc = MockMvcBuilders
-            .webAppContextSetup(context)
-            //.addFilters(springSecurityFilterChain)
-            .build();
-    }
 
     @Test
     public void should_welcome_upon_root_request_with_200_response_code() throws Exception {
