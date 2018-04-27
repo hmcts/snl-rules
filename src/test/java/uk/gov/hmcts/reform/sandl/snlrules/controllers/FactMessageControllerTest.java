66b6eb8a65a9de92459956792cbc3db5a3c62551
Kamil Pakur <k.pakur@kainos.com>
2018-04-27 18:03:08 +0200

    added sonarqube, ignore security checks, fixed stylecheck

--- src/test/java/uk/gov/hmcts/reform/sandl/snlrules/controllers/FactMessageControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlrules/controllers/FactMessageControllerTest.java
@@ -6,13 +6,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import uk.gov.hmcts.reform.sandl.snlrules.drools.FactModification;
 import uk.gov.hmcts.reform.sandl.snlrules.messages.FactMessageHandlerFactory;
-import uk.gov.hmcts.reform.sandl.snlrules.messages.commands.FactCommand;
 import uk.gov.hmcts.reform.sandl.snlrules.messages.commands.InsertFactCommand;
 
 import java.util.ArrayList;
@@ -21,8 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -49,10 +46,10 @@ public class FactMessageControllerTest {
     @Test
     public void should_the_endpoint_works() throws Exception {
 
-        String msg = "{\t\"type\": \"insert-judge\",\n" +
-            "\t\"data\": \"{\\\"id\\\": \\\"j1\\\",\\\"name\\\": \\\"John Smith\\\"}\"}";
+        String msg = "{\t\"type\": \"insert-judge\",\n"
+            + "\t\"data\": \"{\\\"id\\\": \\\"j1\\\",\\\"name\\\": \\\"John Smith\\\"}\"}";
 
-        MvcResult response = mockMvc.perform(put("/msg")
+        MvcResult response = mockMvc.perform(post("/msg")
             .contentType(APPLICATION_JSON_UTF8)
             .content(msg))
             .andExpect(status().isOk()).andReturn();
