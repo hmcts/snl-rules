67648c96aa401310d4cac96ce3515e35e183d1f6
Kamil Pakur <k.pakur@kainos.com>
2018-06-05 10:07:19 +0200

    func tests fixed

--- src/test/functional/java/uk/gov/hmcts/reform/sandl/snlrules/functional/FactMessagesTest.java
+++ src/test/functional/java/uk/gov/hmcts/reform/sandl/snlrules/functional/FactMessagesTest.java
@@ -20,39 +20,80 @@ public class FactMessagesTest {
     @Category(SmokeTest.class)
     public void session_insert_generates_problem() {
 
-        String msgDelete = "{\n"
+        String sessionId = "486ecc4c-61e6-4f79-bd5d-1eb71d999316";
+        String judgeId = "14c8c596-c3aa-4691-87f6-2293f07faf05";
+
+        final String msgSessionDelete = "{\n"
             + "\t\"type\": \"delete-session\",\n"
-            + "\t\"data\": \"{\\\"id\\\":\\\"s523123123\\\","
-            + "\\\"judgeId\\\":\\\"j1\\\",\\\"start\\\":\\\"2048-04-12T15:24:05.255+01:00\\\","
+            + "\t\"data\": \"{\\\"id\\\":\\\"" + sessionId + "\\\","
+            + "\\\"judgeId\\\":\\\"" + judgeId + "\\\",\\\"start\\\":\\\"2048-04-12T15:24:05.255+01:00\\\","
             + "\\\"duration\\\": 3600}\"\n"
             + "}";
 
-        String msgInsert = "{\n"
+        final String msgJudgeDelete = "{\n"
+            + "\t\"type\": \"delete-judge\",\n"
+            + "\t\"data\": \"{"
+            + "\\\"id\\\":\\\"" + judgeId + "\\\","
+            + "\\\"name\\\":\\\"John Smith\\\"}\"\n"
+            + "}";
+
+        final String msgSessionInsert = "{\n"
             + "\t\"type\": \"insert-session\",\n"
-            + "\t\"data\": \"{\\\"id\\\":\\\"s523123123\\\","
-            + "\\\"judgeId\\\":\\\"j1\\\",\\\"start\\\":\\\"2048-04-12T15:24:05.255+01:00\\\","
+            + "\t\"data\": \"{"
+            + "\\\"id\\\":\\\"" + sessionId + "\\\","
+            + "\\\"judgeId\\\":\\\"" + judgeId + "\\\","
+            + "\\\"start\\\":\\\"2048-04-12T15:24:05.255+01:00\\\","
             + "\\\"duration\\\": 3600}\"\n"
+            + "}";
+
+        final String  msgJudgeInsert = "{\n"
+            + "\t\"type\": \"insert-judge\",\n"
+            + "\t\"data\": \"{"
+            + "\\\"id\\\":\\\"" + judgeId + "\\\","
+            + "\\\"name\\\":\\\"John Smith\\\"}\"\n"
             + "}";
 
         // ensure there is no fact with the id already
         given()
             .contentType(ContentType.JSON)
-            .body(msgDelete)
+            .body(msgSessionDelete)
             .when()
-            .post("/msg")
+            .post("/msg?rulesDefinition=Sessions")
             .then()
             .statusCode(200);
 
+        given()
+            .contentType(ContentType.JSON)
+            .body(msgJudgeDelete)
+            .when()
+            .post("/msg?rulesDefinition=Sessions")
+            .then()
+            .statusCode(200);
+
+        // this should trigger "Session Time For the Judge not Available" rule
         JsonPath retrievedFactList = given()
             .contentType(ContentType.JSON)
-            .body(msgInsert)
+            .body(msgSessionInsert)
             .when()
-            .post("/msg")
+            .post("/msg?rulesDefinition=Sessions")
+            .then()
+            .statusCode(200)
+            .and()
+            .extract().jsonPath();
+
+        assertThat(retrievedFactList.getList("type")).contains("Session");
+
+        retrievedFactList = given()
+            .contentType(ContentType.JSON)
+            .body(msgJudgeInsert)
+            .when()
+            .post("/msg?rulesDefinition=Sessions")
             .then()
             .statusCode(200)
             .and()
             .extract().jsonPath();
 
         assertThat(retrievedFactList.getList("type")).contains("Problem");
+        assertThat(retrievedFactList.getList("type")).contains("Judge");
     }
 }
