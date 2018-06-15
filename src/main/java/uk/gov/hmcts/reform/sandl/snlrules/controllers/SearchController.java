639d9e4a88d8c1303415c954e895e8a3e5351da7
Kamil Pakur <k.pakur@kainos.com>
2018-06-15 08:57:18 +0200

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/sandl/snlrules/controllers/SearchController.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlrules/controllers/SearchController.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.reform.sandl.snlrules.controllers;
 
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.rule.QueryResults;
-import org.kie.api.runtime.rule.QueryResultsRow;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,10 +14,7 @@ import uk.gov.hmcts.reform.sandl.snlrules.services.DroolsService;
 import uk.gov.hmcts.reform.sandl.snlrules.services.DroolsServiceFactory;
 
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
