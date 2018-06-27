33a12c5291bacc5ad230d81c61025c6299fe0004
danmerron-sig <dmerron@sportingsolutions.com>
2018-06-27 16:33:48 +0100

    syntax error

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "azurerm_virtual_machine" "rulesengine-vm1" {
   os_profile {
     computer_name  = "${var.env}-rulesengine1"
     admin_username = "${random_string.username.result}"
-    admin_password = "${random_string.username.password}"
+    admin_password = "${random_string.username.result}"
 //    admin_password = "xohpheetahl2aeXa"
   }
 
