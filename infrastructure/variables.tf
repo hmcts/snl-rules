71e8a1cf4686a6fe2f04d28151adc6aef684afc0
danmerron-sig <dmerron@sportingsolutions.com>
2018-06-29 14:02:14 +0100

    fixed vars

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,24 +2,5 @@ variable "location" {
   default = "UK South"
 }
 
-variable "tenant_id" {}
-variable "subscription_id" {}
-variable "client_id" {}
-
-variable "subscription" {}
-
-variable "secret_access_key" {}
-
-#variable "app_settings" {
-#  type = "map"
-#}
-
-
-variable "env" {}
-variable "product" {}
-variable "product_name" {}
-
-variable "jenkins_AAD_objectId" {}
-
 variable "name" {}
 variable "resource_group" {}
--- variables.tf
+++ variables.tf
@@ -2,21 +2,5 @@ variable "location" {
   default = "UK South"
 }
 
-variable "tenant_id" {}
-variable "subscription_id" {}
-variable "client_id" {}
-
-variable "subscription" {}
-
-variable "secret_access_key" {}
-
-#variable "app_settings" {
-#  type = "map"
-#}
-
-
 variable "name" {}
 variable "resource_group" {}
-variable "product_name" {}
-
-variable "jenkins_AAD_objectId" {}
