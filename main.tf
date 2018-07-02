b0e1ac2cb98d4fcf271f4fa14d96c534a41062e2
Dan Merron <dan@dpmerron.co.uk>
2018-07-02 13:12:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,19 @@ module "rulesengine01" {
   source         = "infrastructure"
   name           = "rulesengine01"
   resource_group = "${var.resource_group}"
-  subscription   = "${var.subscription}"
+  subscription_id   = "${var.subscription_id}"
 }
 
 module "rulesengine02" {
   source         = "infrastructure"
   name           = "rulesengine02"
   resource_group = "${var.resource_group}"
-  subscription   = "${var.subscription}"
+  subscription_id   = "${var.subscription_id}"
 }
 
 module "rulesengine03" {
   source         = "infrastructure"
   name           = "rulesengine03"
   resource_group = "${var.resource_group}"
-  subscription   = "${var.subscription}"
+  subscription_id   = "${var.subscription_id}"
 }
