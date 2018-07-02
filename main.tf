9aaf734b95d565002eb995d60e1b614793297c09
danmerron-sig <dmerron@sportingsolutions.com>
2018-07-02 15:49:59 +0100

    changed to 1 vm

--- main.tf
+++ main.tf
@@ -4,17 +4,3 @@ module "rulesengine01" {
   resource_group = "${var.resource_group}"
   subscription_id   = "${var.subscription_id}"
 }
-
-module "rulesengine02" {
-  source         = "infrastructure"
-  name           = "rulesengine02"
-  resource_group = "${var.resource_group}"
-  subscription_id   = "${var.subscription_id}"
-}
-
-module "rulesengine03" {
-  source         = "infrastructure"
-  name           = "rulesengine03"
-  resource_group = "${var.resource_group}"
-  subscription_id   = "${var.subscription_id}"
-}
