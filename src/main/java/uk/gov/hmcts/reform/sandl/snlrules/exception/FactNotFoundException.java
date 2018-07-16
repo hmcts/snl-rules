5faa4a3c6ce6303b2cb7f5c050ded6c4a7ffa157
Kamil Pakur <k.pakur@kainos.com>
2018-07-16 14:46:05 +0200

    SL-1054 checkstyle corrected

--- src/main/java/uk/gov/hmcts/reform/sandl/snlrules/exception/FactNotFoundException.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlrules/exception/FactNotFoundException.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.sandl.snlrules.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such fact")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No such fact")
 public class FactNotFoundException extends RuntimeException {
     public FactNotFoundException(String message) {
         super(message);
--- src/test/java/uk/gov/hmcts/reform/sandl/snlrules/messages/commands/DeleteFactCommandTests.java
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlrules/messages/commands/DeleteFactCommandTests.java
@@ -8,7 +8,6 @@ import org.kie.api.runtime.rule.FactHandle;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.sandl.snlrules.exception.FactCommandException;
 import uk.gov.hmcts.reform.sandl.snlrules.exception.FactNotFoundException;
 import uk.gov.hmcts.reform.sandl.snlrules.model.Room;
 import uk.gov.hmcts.reform.sandl.snlrules.services.DroolsService;
