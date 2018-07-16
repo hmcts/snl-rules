fd17a1eda93b11e9c8b7ae602fbf289ccc255e01
Kamil Pakur <k.pakur@kainos.com>
2018-07-16 12:01:04 +0200

    SL-1054 removed unused code

--- src/main/java/uk/gov/hmcts/reform/sandl/snlrules/model/SessionProposition.java
+++ src/main/java/uk/gov/hmcts/reform/sandl/snlrules/model/SessionProposition.java
@@ -2,12 +2,9 @@ package uk.gov.hmcts.reform.sandl.snlrules.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
-@Data
 @AllArgsConstructor
 public class SessionProposition {
     private String judgeId;
---
+++ src/test/java/uk/gov/hmcts/reform/sandl/snlrules/messages/FactMessageHandlerFactoryTests.java
@@ -0,0 +1,4 @@
+package uk.gov.hmcts.reform.sandl.snlrules.messages;
+
+public class FactMessageHandlerFactoryTests {
+}
