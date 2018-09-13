b03f07609ef0f8820bdd29089a31868af1a47ee3
gary-sedgwick <Gary.Sedgwick@hmcts.net>
2018-09-13 16:47:07 +0100

    Reduce number of performance env rules engine instances from 3 to 1, and reduce size of this VM until we need to do testing with large data volumes again.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -66,7 +66,7 @@ resource "azurerm_virtual_machine" "rulesengine-vm01" {
   location              = "${var.location}"
   resource_group_name   = "${var.resource_group}"
   network_interface_ids = ["${azurerm_network_interface.rulesengine-nic1.id}"]
-  vm_size               = "Standard_E8-4s_v3"
+  vm_size               = "Standard_E2s_v3"
 
   storage_os_disk {
     name              = "${var.name}01-storage"
@@ -84,78 +84,6 @@ resource "azurerm_virtual_machine" "rulesengine-vm01" {
 
   os_profile {
     computer_name  = "${var.name}01"
-    admin_username = "${random_string.username.result}"
-    admin_password = "${random_string.password.result}"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags {
-    environment = "rulesengine"
-  }
-}
-
-resource "azurerm_virtual_machine" "rulesengine-vm02" {
-  name                  = "${var.name}02"
-  location              = "${var.location}"
-  resource_group_name   = "${var.resource_group}"
-  network_interface_ids = ["${azurerm_network_interface.rulesengine-nic2.id}"]
-  vm_size               = "Standard_E8-4s_v3"
-
-  storage_os_disk {
-    name              = "${var.name}02-storage"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  storage_image_reference {
-    publisher = "OpenLogic"
-    offer     = "CentOS"
-    sku       = "7.3"
-    version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "${var.name}02"
-    admin_username = "${random_string.username.result}"
-    admin_password = "${random_string.password.result}"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags {
-    environment = "rulesengine"
-  }
-}
-
-resource "azurerm_virtual_machine" "rulesengine-vm03" {
-  name                  = "${var.name}03"
-  location              = "${var.location}"
-  resource_group_name   = "${var.resource_group}"
-  network_interface_ids = ["${azurerm_network_interface.rulesengine-nic3.id}"]
-  vm_size               = "Standard_E8-4s_v3"
-
-  storage_os_disk {
-    name              = "${var.name}03-storage"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  storage_image_reference {
-    publisher = "OpenLogic"
-    offer     = "CentOS"
-    sku       = "7.3"
-    version   = "latest"
-  }
-
-  os_profile {
-    computer_name  = "${var.name}03"
     admin_username = "${random_string.username.result}"
     admin_password = "${random_string.password.result}"
   }
