f55197fd6b5cf287655d2fea8db485bd38d2dc95
Kamil Pakur <k.pakur@kainos.com>
2018-10-01 17:51:39 +0200

    SL-1722 added shared secrets

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,3 +1,11 @@
 output "frontend_deployment_endpoint" {
   value = "${module.snl-rules.gitendpoint}"
 }
+
+output "sharedResourceGroup" {
+  value = "${local.sharedResourceGroup}"
+}
+
+output "shared_vault_uri" {
+  value = "${data.azurerm_key_vault.snl-shared-vault.vault_uri}"
+}
