c4e2c382ffa9f1081bdbdb49d7ed42e32d81735c
gary-sedgwick <Gary.Sedgwick@hmcts.net>
2018-10-17 10:16:08 +0100

    SL-920
    Updated for new subnets and trimmed TF config

--- createDns.sh
+++ createDns.sh
@@ -10,18 +10,10 @@ echo "uri = $uri"
 echo "ip = $ip"
 echo "consul = $consul"
 
-register_dns () {
-  domain=$1
-  uri=$2
-  ip=$3
+tmp_dir=$(mktemp -d)
+cp "${uri}/consul.json" "$tmp_dir"
+sed -i -e "s/serviceId/$domain/g" "$tmp_dir/consul.json"
+sed -i -e "s/serviceName/$domain/g" "$tmp_dir/consul.json"
+sed -i -e "s/serviceIp/$ip/g" "$tmp_dir/consul.json"
 
-  tmp_dir=$(mktemp -d)
-  cp "${uri}/consul.json" "$tmp_dir"
-  sed -i -e "s/serviceId/$domain/g" "$tmp_dir/consul.json"
-  sed -i -e "s/serviceName/$domain/g" "$tmp_dir/consul.json"
-  sed -i -e "s/serviceIp/$ip/g" "$tmp_dir/consul.json"
-
-  curl -T "$tmp_dir/consul.json" "http://${consul}:8500/v1/agent/service/register"
-}
-
-register_dns $domain $uri $ip
+curl -T "$tmp_dir/consul.json" "http://${consul}:8500/v1/agent/service/register"
